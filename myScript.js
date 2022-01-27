887f60cf0cac0a874bf573a926147f237abe1a2e
Rushi Thakkar <ructhakkar123@gmail.com>
2022-01-27 09:47:25 +0530

    Added FAQ and Footer Section

--- myScript.js
+++ myScript.js
@@ -1,4 +1,5 @@
-// Variables
+////Variables
+// Testimonial Section Variables
 const arrowLeft = document.getElementsByClassName('arrows')[0];
 const arrowRight = document.getElementsByClassName('arrows')[1];
 const clientImg = document.getElementsByClassName('clientImg')[0].firstElementChild;
@@ -11,8 +12,12 @@ const testimonials = [
     {img:"/Portfollio_Creator/Img/client-2.png", review:"This is unbelievable. After using Testimonial Generator my buisness skyrocketed!", name:"Jennifer Musk", profession:"Project Manager@ Microsoft"}
 ]
 clientImg.src = testimonials[i].img;
+// QNA Section Variables
+const que = Array.from(document.getElementsByClassName('que'));
+const ans = Array.from(document.getElementsByClassName('ans'));
 
-//Functions
+////Functions
+// Testimonial Section Functions
 function updateDetail() {
     i++;
     if(i >= testimonials.length){
@@ -25,7 +30,35 @@ function updateDetail() {
     clientProfession.textContent = testimonials[i].profession;
     // console.log(clientImg.src);
 }
+// QNA Section Functions
+function hidhOpendeANS(k){
+    ans[k].style.display = 'none';
+    ans[k].classList.remove('show');
+}
+let k = -1;
+function showANS(i){
+    if(k > -1 && k != i){
+        hidhOpendeANS(k);
+    }
+    if(que[i].nextElementSibling.classList.contains('show')){
+        que[i].nextElementSibling.style.display = 'none';
+        que[i].nextElementSibling.classList.remove('show');
+    }
+    else{
+        que[i].nextElementSibling.style.display = 'block';
+        que[i].nextElementSibling.classList.add('show');
+    }
+    k = i;
+}
 
 // Event Listener
+// Testimonial Section Event Listener
 arrowLeft.addEventListener('click', updateDetail);
 arrowRight.addEventListener('click', updateDetail);
+// QNA Section Event Listener
+que.forEach((question, index) => {
+    // console.log(index);
+    question.addEventListener('click', () => {
+        showANS(index)
+    });
+});
